e73c6172b6cc0d5d7ff2df6007dc42b836093573
Harshal-Dongare <myeducationalarena@gmail.com>
2024-07-01 22:16:01 +0530

    Update Resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: "/functionalSample.pdf",
+            href: "/Harshal-Dongare-Resume.pdf",
             style: "rounded-br-md",
             download: true,
         },
