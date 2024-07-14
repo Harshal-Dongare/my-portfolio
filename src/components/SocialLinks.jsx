45d2052c105feae5abae287c6920fc9dd835bfe6
Harshal-Dongare <myeducationalarena@gmail.com>
2024-07-15 00:55:53 +0530

    Update resume and email

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -31,7 +31,7 @@ const SocialLinks = () => {
                     Mail <HiOutlineMail size={30} />
                 </>
             ),
-            href: "mailto:foo@gmail.com",
+            href: "mailto:harshaldongare.corp@gmail.com",
         },
         {
             _id: 4,
